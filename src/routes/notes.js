446130af2f47121983cecfa8edc1b1c34f2c4a5f
Fredrik Welander <welandfr@arcada.fi>
2026-09-14 13:38:05 +0300

    Code challenge solution

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -11,4 +11,35 @@ router.get('/', (req, res) => {
     res.send(tempData)
 })
 
+router.post('/', (req, res) => {
+    console.log(req.body)
+    // TEMP, ersätts med DB
+    tempData.push(req.body)
+    res.send({
+        msg: "Note created", 
+        id: tempData.length
+    })
+})
+
+router.put('/:id', (req, res) => {
+    console.log(`PATCH ${req.params.id}`)
+    // TEMP, ersätts med DB
+    tempData[req.params.id-1] = req.body
+    res.send({
+        msg: "Note updated", 
+        id: req.params.id,
+        newNote: tempData[req.params.id-1]
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    // TEMP, ersätts med DB
+    tempData.splice(req.params.id-1)
+
+    res.send({
+        msg: "Note deleted", 
+        id: req.params.id
+    })
+})
+
 module.exports = router
